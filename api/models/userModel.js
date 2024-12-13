64c1b01ad3bcb2aea725ecd5a6c616e939dfa6b0
fayeemismail <faheemmuhammed703@gmail.com>
2024-12-13 16:08:10 +0530

    create and add oauth component

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         required: true
+    },
+    profilePicture:{
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1LDOkjkGghywy34d7J_4gpnpXXv9xE_kDZm10nz9-k1K5xD-26oLj_6I&s"
     }
 }, {
     timestamps: true
--- user-admin/src/assets/components/OAuth.jsx
+++ user-admin/src/assets/components/OAuth.jsx
@@ -1,16 +1,44 @@
 // import React from 'react'
-import {GoogleAuthProvider} from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { app } from '../../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../../redux/user/userSlice';
 
 const OAuth = () => {
-    const handleGoogleClick = async () => {
-        try {
-            const provider = new GoogleAuthProvider()
-        } catch (error) {
-            console.log("could not login with google", error)
-        }
+  const dispatch = useDispatch();
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider()
+      const auth = getAuth(app);
+      const result = await signInWithPopup(auth, provider);
+      
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL
+        }),
+      });
+
+      const data = await res.json()
+      dispatch(signInSuccess(data))
+    } catch (error) {
+      console.log("could not login with google", error)
     }
+  }
   return (
-    <button type='button' onClick={handleGoogleClick} className='bg-gradient-to-r from-[#30ab69] via-[#a83434] to-[#fff700] rounded-lg p-3 uppercase text-white hover:opacity-90' >Continue with Google</button>
+    <button
+      type='button'
+      onClick={handleGoogleClick}
+      className='bg-white rounded-lg p-3 uppercase text-black hover:opacity-90 border border-black'
+    >
+      Continue with Google
+    </button>
+
   )
 }
 
